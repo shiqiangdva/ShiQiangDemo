54889a90de72d433d447a9b273039f6a275fe702
史强 <3282035164@qq.com>
2016-11-29 15:43:26 +0800

    写了一些东西

--- MyDemo/app/src/main/java/com/kevin/mydemo/MainActivity.java
+++ MyDemo/app/src/main/java/com/kevin/mydemo/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //随便写的 什么乱七八糟的方法
     }
 }
